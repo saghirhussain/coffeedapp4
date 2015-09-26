b35a40ebc713f7f150af57c694800038cf9bb0c5
saghirhussain <saghirmhussain@gmail.com>
2015-09-26 18:12:17 +0100

    Second commit after work on satics and template files

--- coffeedapp4/settings.py
+++ coffeedapp4/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'coffeedapp4.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(MAIN_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -107,9 +107,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-TEMPLATE_DIRS = (
-    os.path.join(MAIN_DIR, 'templates'),
-    )
 
 STATICFILES_DIRS = (
     os.path.join(MAIN_DIR, 'static'),
